24467670e7be87fac127bdc00a00e2a11984b39c
chakir <chakir.aouad@gmail.com>
2020-08-28 19:31:31 +0200

    navbar when connected

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfileService} from '../services/profile.service';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private profileService: ProfileService, private authService: AuthenticationService) { }
 
   ngOnInit() {
   }
