ca98dfa10c6cca16776a09ee33bb66b374dbcb3b
chakir <chakir.aouad@gmail.com>
2020-09-03 18:55:41 +0200

    handle error with cities' cache + fixed cors error (authorization in the headers)

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -1,7 +1,7 @@
 import {Injectable, OnDestroy} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {map, shareReplay} from 'rxjs/operators';
+import {Observable, of, throwError} from 'rxjs';
+import {catchError, map, shareReplay} from 'rxjs/operators';
 import * as Constants from '../utils/globals';
 
 @Injectable({
@@ -19,7 +19,10 @@ export class CitiesService {
     const headerDict = {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Headers': 'authorization',
+      /*'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'*/
     };
 
     this.requestOptions = {
@@ -36,7 +39,11 @@ export class CitiesService {
         this.cache[url] = this.http.get<any>(url, this.requestOptions).pipe(
           shareReplay(1),
           // @ts-ignore
-          map(obj => obj.cities));
+          map(obj => obj.cities),
+          catchError(err => {
+            delete this.cache[url];
+            return throwError(err);
+          }));
       }
       else {
         console.log('on récupère le cache des villes: ' + url);
--- src/app/utils/helpers/jwt.interceptor.ts
+++ src/app/utils/helpers/jwt.interceptor.ts
@@ -3,13 +3,18 @@ import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/com
 import {Observable} from 'rxjs';
 import {JwtHelperService} from '@auth0/angular-jwt';
 import {LOGGED_IN_KEY} from '../globals';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthenticationService) {
+  }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const currentUser = JSON.parse(localStorage.getItem(LOGGED_IN_KEY));
-    if (currentUser && currentUser.token) {
 
+    if (currentUser && currentUser.token) {
       const helper = new JwtHelperService();
 
       const decodedToken = helper.decodeToken(currentUser.token);
@@ -19,13 +24,18 @@ export class JwtInterceptor implements HttpInterceptor {
       console.log('token expiration date: ' + expirationDate);
       console.log('token isExpired: ' + isExpired);
       console.log('token decodedToken: ' + JSON.stringify(decodedToken));
-
       console.log('JWT Interceptor, cloning request - token = ' + currentUser.token);
-      request = request.clone({
-        setHeaders: {
-          authorization: `Token ${currentUser.token}`
-        }
-      });
+
+      if (isExpired) {
+        this.authService.logout();
+      }
+      else {
+        request = request.clone({
+          setHeaders: {
+            authorization: `Token ${currentUser.token}`
+          }
+        });
+      }
     }
 
     return next.handle(request);
