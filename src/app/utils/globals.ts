3d4428142c8552d3a354374d0522bc29485ac8dd
Mohamed BEN OTHMAN <mohamed.ben-othman@proxym-it.com>
2022-01-15 20:28:05 +0100

    base url

--- src/app/utils/globals.ts
+++ src/app/utils/globals.ts
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 
 /*export const baseURL = 'http://82.165.253.223:3050';*/
-export const baseURL = 'http://localhost:3050';
+// export const baseURL = 'http://localhost:3050';
 
+export const baseURL = "http://82.165.48.48/api";
 
 export const businessURL = `${baseURL}/business`;
 export const expertiseURL = `${baseURL}/expertise`;
@@ -11,12 +12,13 @@ export const photosURL = `${baseURL}/photos`;
 export const userURL = `${baseURL}/user`;
 
 export const uploadPhotoURL = `${baseURL}/files/upload`;
-export const uploadPhotoParam = 'avatar1';
+export const uploadPhotoParam = "avatar1";
 
-export const departmentsURL = '../../assets/json/departments.json';
-export const citiesSearchURL = 'https://vicopo.selfbuild.fr/cherche';
+export const departmentsURL = "../../assets/json/departments.json";
+export const citiesSearchURL = "https://vicopo.selfbuild.fr/cherche";
 
-export const defaultProfilePhoto = '../../assets/images/user_icon_placeholder.svg';
+export const defaultProfilePhoto =
+  "../../assets/images/user_icon_placeholder.svg";
 
 export const UPLOAD_PHOTO_MAX_SIZE = 1_000_000;
 export const DEFAULT_MAX_PHOTO = 3;
@@ -27,8 +29,8 @@ export const USERNAME_MIN_LENGTH = 4;
 
 export const SEARCH_AVAILABLE_ITEMS_PER_PAGE = [1, 3, 6, 25];
 
-export const AUTH0_CLIENT_ID = 'zCs9uYfFgL1mKlDvtTDarJUJ240uKr2S';
-export const AUTH0_DOMAIN = 'dimacsoftware.eu.auth0.com';
+export const AUTH0_CLIENT_ID = "zCs9uYfFgL1mKlDvtTDarJUJ240uKr2S";
+export const AUTH0_DOMAIN = "dimacsoftware.eu.auth0.com";
 
-export const LOGGED_IN_KEY = 'currentUser';
+export const LOGGED_IN_KEY = "currentUser";
 export const CACHE_TTL = 30_000;
